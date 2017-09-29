7f1a4e9503f9658db82b04e9a32970b8b5a7288a
Tim Gremplewski <tim.gremplewski@gmail.com>
2017-09-29 10:11:49 +0200

    WCCS-20 Endpoint to request all sites

--- routes/sites.js
+++ routes/sites.js
@@ -2,62 +2,19 @@
 
 const express = require('express');
 const router = express.Router();
+const logger = require("../lib/Logger");
 const unirest = require("unirest");
 const pageRepository = new (require("../lib/PageRepository"))(unirest);
+const siteRepository = new (require("../lib/SiteRepository"))(unirest);
 
-let sites = {};
-
-(() => {
-    let siteNames = [
-        "B.A. Bildungswissenschaft",
-        "B.A. Kulturwissenschaften mit Fachschwerpunkt Geschichte, Literaturwissenschaft, Philosophie",
-        "B.A. Politikwissenschaft, Verwaltungswissenschaft, Soziologie (PVS)",
-        "B.Sc. Psychologie",
-        "M.A. Bildung und Medien - eEducation",
-        "M.A. Europäische Moderne: Geschichte und Literatur",
-        "M.A. Geschichte Europas - Epochen, Umbrüche, Verflechtungen",
-        "M.A. Governance",
-        "M.A. Philosophie - Philosophie im europäischen Kontext",
-        "M.Sc. Psychologie",
-        "M.A. Soziologie – Zugänge zur Gegenwartsgesellschaft",
-        "B.A. Kulturwissenschaften (ohne Fachschwerpunkt)",
-        "B.A. Soziologie",
-        "M.A. Soziologie: Individualisierung und Sozialstruktur",
-        "Institut für Bildungswissenschaft und Medienforschung"
-    ];
-
-    let states = ["Analyzing", "Analysed", "In Review", "Waiting", "Approved"];
-    let pageTypes = ["FAQ", "Kontakt", "Aktuelles"];
-
-    siteNames.forEach((siteName) => {
-        let siteId = siteName
-            .replace(/\./g, "")
-            .replace(/\s/g, "")
-            .replace(/\(/g, "")
-            .replace(/\)/g, "")
-            .replace(/,/g, "");
-
-        let pages = {};
-        let numberOfPages = Math.floor(Math.random() * 10) + 1;
-        for (let i = 0; i < numberOfPages; i++) {
-            let page = {
-                "url": "http://www.fernuni-hagen.de/ksw/" + siteId + "/page" + i,
-                "status": states[Math.floor(Math.random() * 5)],
-                "pageType": pageTypes[Math.floor(Math.random() * 3)]
-            };
-            pages[page.url.replace(/\//g, "")] = page;
-        }
-
-        sites[siteId] = { "name": siteId , "pages" : pages};
-    });
-})();
-
-router.get('/', (request, response) => {
-    let sitesArray = Object.keys(sites).map((siteName) => sites[siteName]);
-    response.json({
-        "total": sitesArray.length,
-        "sites": sitesArray
-    });
+router.get('/', async (request, response, next) => {
+    try {
+        const sites = await siteRepository.getSites();
+        response.json({ "total": sites.length, "sites": sites });
+    } catch (e) {
+        logger.error(e.message);
+        next(e);
+    }
 });
 
 router.route("/:siteId/pages")
