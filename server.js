48c5e738640e58f7475533d60fccc1e20ea2eb1e
Tim Gremplewski <tim.gremplewski@gmail.com>
2017-12-21 11:42:53 +0100

    Increase body parser limit to prevent 413 status

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const pages = require('./routes/pages');
 const PORT = 52629;
 
 app.use(logger('dev'));
-app.use(bodyParser.json());
+app.use(bodyParser.json({"limit": "1 mb"}));
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(cookieParser());
 
